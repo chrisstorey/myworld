86f239cd799a11e469dd56bce964e12db3c66c1e
chrisstorey <chris.storey@working-age.digital.dwp.gov.uk>
2021-12-26 21:03:06 +0000

    CS: move to other machine

--- make_people.py
+++ make_people.py
@@ -22,24 +22,12 @@ GENERAL_TABLE = cfg["database"]["general_table"]
 PEOPLE_TABLE = cfg["database"]["people_table"]
 
 
-def create_dataframe():
-    pd.DataFrame(columns=[
-        'My_World_person_UUID',
-        "first_name",
-        "last_name",
-        "gender",
-        "adult",
-        "date_of_birth",
-        "age",
-        "married_to_UUID",
-        "nationality_for_name",
-        "is_claimant",
-        'My_World_premise_UUID'])
+
 
 
 def create_first_name(nationality: str, gender: str):
     fake = Faker(nationality)
-    if gender == "F":
+    if gender == "Female":
         first_name = fake.first_name_female()
     else:
         first_name = fake.first_name_male()
@@ -52,12 +40,16 @@ def create_last_name(nationality: str):
     return last_name
 
 
-def create_person(My_World_premise_ID: str, type_of_person: str, gender: str, married: bool):
-    My_World_premise_ID = My_World_premise_ID
-    My_World_person_ID = str(uuid.uuid4())
+def create_person(my_world_premise_id: str, type_of_person: str, gender: str, married_surname: str = None) -> object:
+    my_world_premise_id = my_world_premise_id
+    my_world_person_id = str(uuid.uuid4())
     nationality = people.nationality.country_name()
     first_name = create_first_name(nationality, gender)
-    last_name = create_last_name(nationality)
+    if married_surname != None:
+        last_name = married_surname
+    else:
+        last_name = create_last_name(nationality)
+
     if type_of_person == "lead-adult":
         adult = True
         dob = people.age.dob_working_age()
@@ -70,15 +62,27 @@ def create_person(My_World_premise_ID: str, type_of_person: str, gender: str, ma
     else:
         adult = False
         dob = people.age.dob_under18()
-    return My_World_premise_ID, My_World_person_ID, \
-           nationality, first_name, \
-           last_name, type_of_person, dob
+    return my_world_premise_id, my_world_person_id, nationality, first_name, last_name, \
+           type_of_person, dob, adult, married_surname
 
 
 # main function
 household_data = create_connection(DATABASE)
 household_dataframe = pd.read_sql_query("""SELECT * from """ + GENERAL_TABLE, household_data)
 print("Length of dataframe : ", len(household_dataframe))
+
+people_dataframe.DataFrame(columns=[
+    'My_World_person_UUID',
+    "first_name",
+    "last_name",
+    "gender",
+    "adult",
+    "date_of_birth",
+    "age",
+    "married_to_UUID",
+    "nationality_for_name",
+    "is_claimant",
+    'My_World_premise_UUID'])
 for i in tqdm(range(len(household_dataframe))):
     print(i, household_dataframe.My_World_premise_UUID[i],
           household_dataframe.household_type[i],
@@ -91,15 +95,45 @@ for i in tqdm(range(len(household_dataframe))):
           "Non-Deps =", household_dataframe.non_dep[i], people.nationality.country_name()
           )
 
-# test_nat = people.nationality.country_name()[0]
-test_gender = people.gender.gender()[0]
-# print(test_nat)
-# print(create_first_name(test_nat, test_gender))
-# print(create_last_name(test_nat))
+    for male in range(household_dataframe.adult_m[i]):
+        create_person(my_world_premise_id[i], "Adult", "Male")
+        people_dataframe.append()
+        'My_World_person_UUID',
+        "first_name",
+        "last_name",
+        "gender",
+        "adult",
+        "date_of_birth",
+        "age",
+        "married_to_UUID",
+        "nationality_for_name",
+        "is_claimant",
+        'My_World_premise_UUID'])
+        for female adults
+        if married:
+            create_person(my_world_premise_id[i], "Adult", "Female", last_name)
+        elif:
+        create_person(my_world_premise_id[i], "Adult", "Female")
+
+        for children
+        create_person(my_world_premise_id[i], "child", random.choice(["Male", "Female"]), last_name)
+
+        for maleover65
+        create_person(my_world_premise_id[i], "65", "Male")
+
+        for femaleover65
+        create_person(my_world_premise_id[i], "65", "Female")
 
 
-person = create_person("bf6c4706-3f78-4748-944f-71566009cb72", "adult", test_gender, True)
-print(person)
+
+        # test_nat = people.nationality.country_name()[0]
+        test_gender = people.gender.gender()[0]
+        # print(test_nat)
+        # print(create_first_name(test_nat, test_gender))
+        # print(create_last_name(test_nat))
+
+        person = create_person("bf6c4706-3f78-4748-944f-71566009cb72", "adult", test_gender, True)
+        print(person)
 
 # for i in tqdm(range(len(household_dataframe))):
 #     household_dataframe['outcode'][i] = data['result'][0]['outcode']
